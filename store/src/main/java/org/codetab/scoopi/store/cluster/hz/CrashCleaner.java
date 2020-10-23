e637f9d476eb108929217a6315dbacd1b452134f
Maithilish <maithilish@gmail.com>
2020-10-23 19:41:49 +0530

    fix crash clean
    	remove crashed member from stack in next cycle if no pending taken jobs
            in jobStore takeJob throw separate exception if jobs list is empty so that
            jobRunner waits before retry

--- store/src/main/java/org/codetab/scoopi/store/cluster/hz/CrashCleaner.java
+++ store/src/main/java/org/codetab/scoopi/store/cluster/hz/CrashCleaner.java
@@ -1,5 +1,6 @@
 package org.codetab.scoopi.store.cluster.hz;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
@@ -44,62 +45,64 @@ public class CrashCleaner {
     }
 
     public boolean resetCrashedJobs() {
-        Stack<String> crashedMembers = membershipListener.getCrashedMembers();
-        if (crashedMembers.isEmpty()) {
-            LOG.debug("no crashed members");
-            return false;
-        }
         String leader = cluster.getLeader();
         if (!leader.equals(cluster.getMemberId())) {
-            LOG.debug("not leader, don't reset crashed jobs. leader is {}",
-                    leader);
             return false;
         }
 
-        while (!crashedMembers.isEmpty()) {
-            String crashedMemberId = crashedMembers.peek();
+        Stack<String> crashedMembers = membershipListener.getCrashedMembers();
+        if (crashedMembers.isEmpty()) {
+            return false;
+        } else {
+            LOG.debug("crashed members {}",
+                    Arrays.toString(crashedMembers.toArray()));
+            LOG.debug("i am {} leader, reset taken jobs",
+                    cluster.getShortId(leader));
+        }
 
-            List<Long> takenJobs = takenJobsMap.values().stream().filter(
-                    p -> p.isTaken() && p.getMemberId().equals(crashedMemberId))
-                    .map(ClusterJob::getJobId).collect(Collectors.toList());
+        String crashedMemberId = crashedMembers.peek();
 
-            if (takenJobs.isEmpty()) {
-                // no taken job by crashed node, remove it
-                crashedMembers.pop();
-            } else {
-                LOG.info("reset {} jobs taken by {}", takenJobs.size(),
-                        cluster.getShortId(crashedMemberId));
-                TransactionContext tx = hz.newTransactionContext(txOptions);
-                try {
-                    tx.beginTransaction();
-                    TransactionalList<ClusterJob> txJobsList =
-                            tx.getList(DsName.JOBS_LIST.toString());
-                    TransactionalMap<Long, ClusterJob> txTakenJobsMap =
-                            tx.getMap(DsName.TAKEN_JOBS_MAP.toString());
+        List<Long> takenJobs = takenJobsMap.values().stream().filter(
+                p -> p.isTaken() && p.getMemberId().equals(crashedMemberId))
+                .map(ClusterJob::getJobId).collect(Collectors.toList());
 
-                    for (Long jobId : takenJobs) {
-                        LOG.debug("reset taken job {}", jobId);
-                        ClusterJob cJob = txTakenJobsMap.remove(jobId);
-                        cJob.setTaken(false);
-                        cJob.setMemberId(null);
-                        txJobsList.add(cJob);
-                    }
-                    tx.commitTransaction();
-                    // done, remove the crashed node
-                    String removedId = crashedMembers.pop();
-                    LOG.debug(
-                            "reset taken job completed, crashed member {} removed",
-                            cluster.getShortId(removedId));
-                } catch (Exception e) {
-                    tx.rollbackTransaction();
-                    LOG.warn("could not reset jobs taken by {}, {}",
-                            cluster.getShortId(crashedMemberId),
-                            e.getLocalizedMessage());
-                    LOG.debug("{}", e);
+        if (takenJobs.isEmpty()) {
+            // no taken job by crashed node, remove it
+            crashedMembers.pop();
+            LOG.debug("no taken jobs by {}, crashed member removed",
+                    cluster.getShortId(crashedMemberId));
+            return false;
+        } else {
+            LOG.info("reset {} jobs taken by {}", takenJobs.size(),
+                    cluster.getShortId(crashedMemberId));
+            TransactionContext tx = hz.newTransactionContext(txOptions);
+            try {
+                tx.beginTransaction();
+                TransactionalList<ClusterJob> txJobsList =
+                        tx.getList(DsName.JOBS_LIST.toString());
+                TransactionalMap<Long, ClusterJob> txTakenJobsMap =
+                        tx.getMap(DsName.TAKEN_JOBS_MAP.toString());
+
+                for (Long jobId : takenJobs) {
+                    LOG.debug("reset taken job {}", jobId);
+                    ClusterJob cJob = txTakenJobsMap.remove(jobId);
+                    cJob.setTaken(false);
+                    cJob.setMemberId(null);
+                    txJobsList.add(cJob);
                 }
+                tx.commitTransaction();
+                LOG.debug(
+                        "reset taken jobs finished, crashed node will be removed in next cycle");
+                return true;
+            } catch (Exception e) {
+                tx.rollbackTransaction();
+                LOG.warn("could not reset jobs taken by {}, {}",
+                        cluster.getShortId(crashedMemberId),
+                        e.getLocalizedMessage());
+                LOG.debug("{}", e);
+                return false;
             }
         }
-        return true;
     }
 
     public void clearDanglingJobs() {
