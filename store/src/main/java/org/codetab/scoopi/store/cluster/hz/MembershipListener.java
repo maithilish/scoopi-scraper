88fc0a44050d8934170871e90ce3617a1f409fb2
Maithilish <maithilish@gmail.com>
2020-10-31 19:34:36 +0530

    refactor Crash Cleaner
    	extract CrashedMembers stack from MembershipListener to CopyOnWriteArraySet

--- store/src/main/java/org/codetab/scoopi/store/cluster/hz/MembershipListener.java
+++ store/src/main/java/org/codetab/scoopi/store/cluster/hz/MembershipListener.java
@@ -1,8 +1,6 @@
 package org.codetab.scoopi.store.cluster.hz;
 
-import java.util.Arrays;
-import java.util.Stack;
-
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.apache.logging.log4j.LogManager;
@@ -16,25 +14,21 @@ public class MembershipListener
 
     private static final Logger LOG = LogManager.getLogger();
 
-    private Stack<String> crashedMembers = new Stack<>();
-
-    public Stack<String> getCrashedMembers() {
-        return crashedMembers;
-    }
+    @Inject
+    private CrashedMembers crashedMembers;
 
     @Override
     public void memberAdded(final MembershipEvent membershipEvent) {
         String addedMemberId = membershipEvent.getMember().getUuid().toString();
-        LOG.info("member joined cluster {}", addedMemberId);
+        LOG.info("member {} joined the cluster", addedMemberId);
     }
 
     @Override
     public void memberRemoved(final MembershipEvent membershipEvent) {
         String crashedMemberId =
                 membershipEvent.getMember().getUuid().toString();
-        LOG.info("member {} left cluster", crashedMemberId);
-        crashedMembers.push(crashedMemberId);
-        LOG.debug("crashed members {}",
-                Arrays.toString(crashedMembers.toArray()));
+        LOG.info("member {} left the cluster", crashedMemberId);
+        crashedMembers.add(crashedMemberId);
     }
+
 }
